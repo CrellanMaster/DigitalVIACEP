7544227239d6145c99116f51e271c933588ec070
Sandro <devsandrounity@gmail.com>
2023-04-25 23:32:39 -0300

    Atribuídas funcoes para tratamento de CEP
    Adicionadas funcoes removeHyphen e AddHyphen para tratamento do CEP de acordo com a API que estiver disponível.

--- src/Search.php
+++ src/Search.php
@@ -8,19 +8,37 @@ class Search
     private $urlViaCep = "https://viacep.com.br/ws/";
     private $urlWideNet = "https://cdn.apicep.com/file/apicep/";
 
+    public function removeHyphen($zipCode): string
+    {
+        $zipCodeFiltred = preg_replace('/[^0-9]/im', '', $zipCode);
+
+        return $zipCodeFiltred;
+    }
+
+    public function addHyphen($zipCode): string
+    {
+        $formatedCep = '';
+        $zipCodeFiltred = preg_replace('/[^0-9]/im', '', $zipCode);
+        if (strlen($zipCodeFiltred) === 8) {
+            $formatedCep = substr_replace($zipCode, "-", 5, 0);
+        } else {
+            $formatedCep = $zipCode;
+        }
+
+        return $formatedCep;
+    }
+
     public function getAddresFromZipCode(string $zipCode): array
     {
 
-        $zipCodeFiltred = preg_replace('/[^0-9]/im', '', $zipCode);
 
-
-        if (is_array($get = json_decode(file_get_contents($this->urlViaCep . $zipCodeFiltred . "/json"), true))) {
+        if (is_array($get = json_decode(file_get_contents($this->urlViaCep . $this->removeHyphen($zipCode) . "/json"), true))) {
             $data = $get;
-        } else if (is_array($get = json_decode(file_get_contents($this->urlWideNet . $zipCode . ".json"), true))) {
+        } else if (is_array($get = json_decode(file_get_contents($this->urlWideNet . $this->addHyphen($zipCode) . ".json"), true))) {
             $data = $get;
         }
 
-        return $get;
+        return $data;
     }
 }
 
